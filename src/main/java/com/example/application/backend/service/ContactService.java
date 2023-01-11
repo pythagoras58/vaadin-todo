4b81c9ef29f479a0b636405f566780b6d1ef496e
pythagoras58 <solomondanso58@gmail.com>
2023-01-11 23:26:12 +0000

    Service - 4

--- src/main/java/com/example/application/backend/service/ContactService.java
+++ src/main/java/com/example/application/backend/service/ContactService.java
@@ -27,4 +27,8 @@ public class ContactService {
     public long count(){
         return contactRepository.count();
     }
+
+    public void delete(ContactEntity contact){
+        contactRepository.delete(contact);
+    }
 }
