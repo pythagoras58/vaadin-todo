ab6fbfec2c2a014042466d64fb43565e4c6063d5
pythagoras58 <solomondanso58@gmail.com>
2023-01-11 23:32:49 +0000

    Service - Company service 3

--- src/main/java/com/example/application/backend/service/CompanyService.java
+++ src/main/java/com/example/application/backend/service/CompanyService.java
@@ -1,7 +1,10 @@
 package com.example.application.backend.service;
 
+import com.example.application.backend.entities.CompanyEntity;
 import com.example.application.backend.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CompanyService {
@@ -11,5 +14,7 @@ public class CompanyService {
         this.companyRepository = companyRepository;
     }
 
-
+    public List<CompanyEntity> findAll(){
+        return companyRepository.findAll();
+    }
 }
