c5ccaacb8b213c50b3c066a295b480c813dee499
pythagoras58 <solomondanso58@gmail.com>
2023-01-11 21:47:37 +0000

    Dependencies

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * The entry point of the Spring Boot application.
@@ -16,6 +17,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Theme(value = "mytodo")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
+//@ComponentScan
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
