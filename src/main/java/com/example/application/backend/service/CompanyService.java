709db42262bccafa028e17003ba7cf5f2e2c9a5c
pythagoras58 <solomondanso58@gmail.com>
2023-01-11 23:31:55 +0000

    Service - Company service 2

--- src/main/java/com/example/application/backend/service/CompanyService.java
+++ src/main/java/com/example/application/backend/service/CompanyService.java
@@ -1,7 +1,15 @@
 package com.example.application.backend.service;
 
+import com.example.application.backend.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompanyService {
+    private CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+
 }
