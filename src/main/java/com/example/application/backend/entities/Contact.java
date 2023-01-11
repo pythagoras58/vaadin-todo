e67cadb6ef6ad4508cffb8552a5d2ccb8f5718c6
pythagoras58 <solomondanso58@gmail.com>
2023-01-11 23:03:51 +0000

    Entities - done

---
+++ src/main/java/com/example/application/backend/entities/Contact.java
@@ -0,0 +1,81 @@
+package com.example.application.backend.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Contact extends AbstractEntity{
+
+    public enum Status{
+        ImportedLead, NotContacted, Contacted, Customer, ClosedLost
+    }
+    @NotEmpty
+    private String firstName = "";
+
+    @NotEmpty
+    private String lastName = "";
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    @NotNull
+    @JsonIgnoreProperties({"employees"})
+    private Company company;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Email
+    @NotEmpty
+    @NotNull
+    private String email = "";
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
