22c3c1811d3997008fe1062d89149ecd186c8594
pythagoras58 <solomondanso58@gmail.com>
2023-01-11 23:40:02 +0000

    Service - Company service 4

--- src/main/java/com/example/application/backend/service/CompanyService.java
+++ src/main/java/com/example/application/backend/service/CompanyService.java
@@ -5,9 +5,12 @@ import com.example.application.backend.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class CompanyService {
+    private static final Logger LOGGER = Logger.getLogger(ContactService.class.getName());
     private CompanyRepository companyRepository;
 
     public CompanyService(CompanyRepository companyRepository) {
@@ -16,5 +19,14 @@ public class CompanyService {
 
     public List<CompanyEntity> findAll(){
         return companyRepository.findAll();
+    }
+
+    public void save(CompanyEntity company){
+        if(company == null){
+            LOGGER.log(Level.SEVERE, "Company Cannot Be Null");
+            return;
+        }
+
+        companyRepository.save(company);
     }
 }
