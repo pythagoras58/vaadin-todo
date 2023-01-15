2f91336bad2a34e85baa80906d418825f581168d
pythagoras58 <solomondanso58@gmail.com>
2023-01-15 15:06:10 +0000

    Contact Form - 5

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -33,6 +33,8 @@ public class MainView extends HorizontalLayout {
         configureGrid();
         configureFilter();
 
+        ContactForm form = new ContactForm();
+
         add(filterText, grid);
 
         updateList();
