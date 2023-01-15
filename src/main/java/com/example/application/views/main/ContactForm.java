68a509d64a89d6b11452b01adb6085979f252bfb
pythagoras58 <solomondanso58@gmail.com>
2023-01-15 14:39:38 +0000

    Contact Form - 2

--- src/main/java/com/example/application/views/main/ContactForm.java
+++ src/main/java/com/example/application/views/main/ContactForm.java
@@ -1,4 +1,33 @@
 package com.example.application.views.main;
 
-public class ContactForm {
+import com.example.application.backend.entities.CompanyEntity;
+import com.example.application.backend.entities.ContactEntity;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+
+public class ContactForm extends FormLayout {
+    TextField firstName = new TextField("First name"); 
+    TextField lastName = new TextField("Last name");
+    EmailField email = new EmailField("Email");
+    ComboBox<ContactEntity.Status> status = new ComboBox<>("Status");
+    ComboBox<CompanyEntity> company = new ComboBox<>("Company");
+    Button save = new Button("Save"); 
+    Button delete = new Button("Delete");
+    Button close = new Button("Cancel");
+    public ContactForm() {
+        addClassName("contact-form"); 
+        add(firstName,
+                lastName,
+                email,
+                company,
+                status,
+                createButtonsLayout()); 
+    }
+
+    private Component createButtonsLayout() {
+    }
 }
